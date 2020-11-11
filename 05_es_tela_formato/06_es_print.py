0bcc8a6bdb4156f6a5942fd316f1b2a7306214be
Luiz Fernando L. R. Silva <lflopes@eq.ufrj.br>
2020-11-11 10:21:41 -0300

    Atualizando tutorial sobre comando print

--- 05_es_tela_formato/06_es_print.py
+++ 05_es_tela_formato/06_es_print.py
@@ -1,13 +1,27 @@
 """
  Autor : LF Silva
  Data  : 10/11/2020
+         11/11/2020
 
  Comando print
 
- Absolutamente importante para criar interfaces por comando de linha
+ Absolutamente importante para criar interfaces por comando de linha!
+
+ print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+ em que:
+
+ * objects - o que deve ser apresentado em tela (ou arquivo). * indica que pode 
+             existir mais de um objeto.
+ * sep -     como os objetos são separados. Valor padrão: ' '
+ * end -     o que é adicionado ao fim do objeto apresentado. Valor padrão: '\n'
+ * file -    deve ser um objeto com método write(string).
+             Se omitido, sys.stdout (saída em tela ) é usado.
+ * flush -   Se True, a saída imediata é forçada.  Valor padrão: False
 
  Mais detalhes em:
  https://realpython.com/python-print/
+ https://www.programiz.com/python-programming/methods/built-in/print
 """
 
 # O comando print, por padrão, sempre inclui um identificador
