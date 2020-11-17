529dee1e91998236e83a319c8d865ce4011b31c5
Luiz Fernando L. R. Silva <lflopes@eq.ufrj.br>
2020-11-17 14:44:39 -0300

    atualizando tutorial sobre exception

--- 10_excecoes/01_excecoes_basico.py
+++ 10_excecoes/01_excecoes_basico.py
@@ -1,36 +1,65 @@
-#
-# Autor: LF Silva
-# Data : 13/04/2020
-#
-# Uso de exceções para análise e avaliação de erros
+""" Autor: LF Silva
+ Data : 13/04/2020
+        17/11/2020
 
-# Existem situações onde sabe-se, a priori, que o código pode falhar de acordo
-# com uma exceção específica. Imagine que você montou todo o seu código e tentou
-# se cercar de todas as possibilidades para a execução. Contudo, existe um caso
-# em que ele pode falhar sendo, portanto, uma exceção. Um exemplo disso é a
-# divisão por zero ou a leitura de um arquivo que não existe.
+ Uso de exceções para análise e avaliação de erros
 
-# Vamos analisar os comandos abaixo
-#
-# >>> n = int(input("Entre com um número: "))
-# >>> Entre com um número: 23.5
-#  Traceback (most recent call last):
-#    File "<stdin>", line 1, in <module>
-#  ValueError: invalid literal for int() with base 10: '23.5'
-#
-# O código em si não restringiu o tipo de variável (no caso, um inteiro). E se o
-# usuário insere um ponto flutuante? Ou escreve por extenso como uma string?
+ Para pensar: Se você estiver na beira de um precipício e sabe que
+ precisa pular. Você prefere pular logo ou é melhor olhar antes,
+ perceber que é alto e usar um paraquedas?
 
-# Vamos usar a exceção
-while True:
-    try:
-        n = input("Entre com um número: ")
-        n = int(n)
-        break
-    except ValueError:
-        print("No. não é um inteiro! Por favor, tente novamente...")
-print("Ótimo, você inseriu um no inteiro!")
+ Um script em Python pode interromper sua execução por dois motivos: erro
+ na sintaxe ou por exceção. Mas qual a diferença?
 
-# Caso o script acuse um erro ao tentar converter o valor da variável para
-# o tipo inteiro, pode-se usar a exceção para tentar novamente. Portanto, 
-# evita-se a interrupção na execução do script. 
+ - erro de sintaxe:
+
+ >>> print(0/0))
+ File "<stdin>", line 1
+    print( 0 / 0 ))
+                  ^
+ SyntaxError: invalid syntax
+
+ - erro de execeção:
+
+ >>> print(0/0)
+ Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ ZeroDivisionError: integer division or modulo by zero
+
+ Portanto, existem situações onde sabe-se, a priori, que o código pode falhar
+ de acordo com uma exceção específica. Imagine que você montou todo o seu
+ código e tentou se cercar de todas as possibilidades para a execução. Contudo,
+ existe um caso em que ele pode falhar sendo, portanto, uma exceção. Um exemplo
+ disso é a divisão por zero ou a leitura de um arquivo que não existe.
+
+ Nessa condição, o script é interrompido pelo erro. Mas e se existisse uma
+ forma de avaliar se erro existe e dar uma segunda chance usando outra
+ estratégia? Essa é uma vantagem do uso de exceções.
+
+ Além disso, muitas vezes as mensagens de erro não apresentam mensagem de erro
+ muito explicativas. Por isso, o desenvolvedor pode lançar (raise) uma
+ mensagem, um aviso customizado. Essa é outra vantagem no uso de excecões.
+"""
+import sys
+
+# Teste 1
+# digamos que, por uma acaso da vida, o seu resultado deu 8. Você pode avaliar
+# se o mesmo é consistente e lançar (raise) uma mensagem caso não seja.
+# O comando Exception interrompe a execução do script e informa a mensagem com
+# classificação Exception.
+# INICIO COMENTARIO 1
+x = 8
+if(x > 5):
+    raise Exception(f"x não pode exceder 5. O valor obtido foi {x}")
+
+# FIM COMENTARIO 1
+
+# Se se formos assertivos em relação a um possível erro e já se precaver
+# de determinada situação. Para o comando assert:
+# Se a condição for True, o script continua
+# Se a condição for False, o script lança o AssertionError
+# INICIO COMENTARIO 2
+# assert('linux' in sys.platform), \
+#     "Esse programa só pode ser executado em Linux!"
+
+# FIM COMENTARIO 2
--- 10_excecoes/02_excecoes_avanc.py
+++ 10_excecoes/02_excecoes_avanc.py
@@ -1,18 +1,95 @@
-#
-# Autor: LF Silva
-# Data : 13/04/2020
-#
-# Uso de exceções para análise e avaliação de erros
-# Erros possíveis:
-# - ValueError : erro relacionado a valores
-# - IOError    : erro relacionado a leitura e escrita em arquivos
+"""
+ Autor: LF Silva
+ Data : 13/04/2020
 
-while True:
+ Ainda é possível criar modos de avaliação de erros em blocos
+
     try:
-        # caso o usuário entre 
-        n = input("Entre com um número: ")
-        n = int(n)
-        break
-    except ValueError:
-        print("No. não é um inteiro! Por favor, tente novamente...")
-print("Ótimo, você inseriu um no inteiro!")
+        tenta esse comando
+    except:
+        se der algum erro, tente esse comando
+
+ Mais informações:
+ https://realpython.com/python-exceptions/
+ https://docs.python.org/3/library/exceptions.html
+ https://dbader.org/blog/python-assert-tutorial
+"""
+import sys
+
+
+# vamos montar uma função simples de avaliação de sistema operacional
+def linux_int():
+    assert("linux" in sys.platform), "Mano, já te disse que só roda em Linux"
+
+
+if __name__ == '__main__':
+    # INÍCIO COMENTÁRIO 1
+    # try:
+    #     linux_int()
+    # except:
+    #     print("Função exclusiva para linux não foi executada.")
+    # FIM COMENTÁRIO 1
+
+    # mas não foi possível ver a sua mensagem customisada de erro
+    # INÍCIO COMENTÁRIO 2
+    # try:
+    #     linux_int()
+    # except AssertionError as linux_erro:
+    #     print(linux_erro)
+    #     print("Função exclusiva para linux não foi executada.")
+    # FIM COMENTÁRIO 2
+
+    # Existem vários tipos de erros de exceção. Busque informação
+    # no manual do Python para ver as possibilidades.
+    # FileNotFoundError é, bem, para tentativa de leitura de
+    # arquivos que não foram encontrados.
+    # INÍCIO COMENTÁRIO 3
+    # try:
+    #     with open('file.log') as file:
+    #         read_data = file.read()
+    # except FileNotFoundError as fnf_erro:
+    #     print(fnf_erro)
+    # FIM COMENTÁRIO 3
+
+    # E se combinarmos os erros? Funciona?
+    # INÍCIO COMENTÁRIO 4
+    # try:
+    #     linux_int()
+    #     with open('file.log') as file:
+    #         read_data = file.read()
+    # except FileNotFoundError as fnf_erro:
+    #     print(fnf_erro)
+    # except AssertionError as linux_erro:
+    #     print(linux_erro)
+    #     print("Função exclusiva para linux não foi executada.")
+    # FIM COMENTÁRIO 4
+
+    # Importante!
+    # - o bloco try é executado até atingir a primeira exceção.
+    # - você pode definir como o script responde à execeção dentro
+    #   de except
+    # - usar exceções múltiplas pode ficar confuso...
+
+    # Vamos aprender o bloco try: else:
+    # INÍCIO COMENTÁRIO 5
+    try:
+        linux_int()
+    except AssertionError as linux_erro:
+        print(linux_erro)
+        print("Função exclusiva para linux não foi executada.")
+    else:
+        try:
+            with open('file.log') as file:
+                read_data = file.read()
+        except FileNotFoundError as fnf_erro:
+            print(fnf_erro)
+    # FIM COMENTÁRIO 5
+
+    # try:
+    #    execute comandos
+    # except:
+    #    execute se encontrar exceções
+    # else:
+    #    execute se não encontrar exeções
+    # finally:
+    #    execute de qualquer maneira
